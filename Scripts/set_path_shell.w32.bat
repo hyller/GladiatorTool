29d13cd9b68df83487c8902e6dd97ce999ae27a1
hyller <haile.jiang@gmail.com>
2015-07-07 08:59:59 +0800

    add set path shell win32

---
+++ Scripts/set_path_shell.w32.bat
@@ -0,0 +1,5 @@
+@echo off
+SET PATH=%GLADIATOR_HOME%\ThirdParty\shell.w32-ix86;%PATH%
+echo add path %GLADIATOR_HOME%\ThirdParty\shell.w32-ix86
+
+
--- Scripts/start_winshell.bat
+++
@@ -1,5 +0,0 @@
-@echo on
-
-SET PATH=%GLADIATOR_HOME%\ThirdParty\shell.w32-ix86;%PATH%
-
-
