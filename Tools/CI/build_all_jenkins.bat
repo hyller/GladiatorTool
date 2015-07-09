ed086a6dd090ad6e3fd124cfe40afaf41566a604
hyller <haile.jiang@gmail.com>
2015-07-09 12:32:21 +0800

    green svn for ci

--- Tools/CI/build_all_jenkins.bat
+++ Tools/CI/build_all_jenkins.bat
@@ -1,4 +1,12 @@
 set work_dir=%WORKSPACE%
 
+cd %work_dir%
+
+rem green work copy
+svn revert -R .
+for /f "usebackq tokens=2*" %%i in (`svn status ^| findstr /r "^\?"`) do svn delete --force "%%i %%j"
+svn up
+
+rem start build
 call %work_dir%\Tools\CI\build_all.bat
 
