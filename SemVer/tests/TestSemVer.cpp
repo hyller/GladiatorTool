a66794b6b0bc9b742445b7f8f72698177a2773c3
hyller <haile.jiang@gmail.com>
2015-07-22 22:29:39 +0800

    [semver] blabla

--- SemVer/tests/TestSemVer.cpp
+++ SemVer/tests/TestSemVer.cpp
@@ -1,5 +1,5 @@
 #include "CppUTest/TestHarness.h"
-
+#include "semver.h"
 
 TEST_GROUP(TestSemVer)
 { 
@@ -16,7 +16,18 @@ void teardown()
 
 TEST(TestSemVer, TestGAsciiPrintAsciiByindexNormal) 
 {
-  CHECK_EQUAL( 1, 1 );
+  tSemverVersion ver;
+  char str[10] = "1.2.3";
+  
+  ver.major = 0;
+  ver.minor = 0;
+  ver.patch = 0; 
+  
+  SemVer_ConvertFromStr(&ver, str);
+  
+  CHECK_EQUAL( 1, ver.major );
+  CHECK_EQUAL( 2, ver.minor );
+  CHECK_EQUAL( 3, ver.patch );
 }
 
 TEST(TestSemVer, TestGAsciiPrintAsciiByindex128) 
