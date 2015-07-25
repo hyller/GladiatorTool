b8746bb6e3e0c833c645582da36fb4ecf90eb0f0
hyller <haile_jiang@163.com>
2015-07-25 12:20:10 +0800

    [scripts] modify igit

--- Scripts/igit.bat
+++ Scripts/igit.bat
@@ -4,12 +4,20 @@ setlocal
 REM Usage: igit store_user
 
 if "%1" == "store_user" goto STORE_USER
+if "%1" == "clean_unversion" goto CLEAN_UNVERSION
 
 echo "error input"
 goto END
 
 :STORE_USER
-echo git config --global credential.helper store
+git config --global credential.helper store
+goto END
+
+:CLEAN_UNVERSION
+for /f "usebackq tokens=2*" %%i in (`git status -s ^| findstr /r "^\??"`) do rm -rf "%%i"
+goto END
+
+
 
 :END
 endlocal
