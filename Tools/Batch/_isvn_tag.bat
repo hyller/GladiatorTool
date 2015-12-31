eac1e301091dbb4e42d0520c89e57c0cdeeee134
hyller <haile.jiang@gmail.com>
2015-12-31 16:30:08 +0800

    [GladiatorFirmware] update

--- Tools/Batch/_isvn_tag.bat
+++ Tools/Batch/_isvn_tag.bat
@@ -7,7 +7,7 @@ call %GLADIATOR_HOME%\Tools\Batch\_get_cd_name.bat
 if %VERSION_STR% == "" goto Error
 if %CURRENT_DIR_NAME% == "" goto Error
 
-echo call isvn tag %CURRENT_DIR_NAME%_%VERSION_STR%
+call isvn tag %CURRENT_DIR_NAME%_%VERSION_STR%
 @echo off
 goto EXIT
 
