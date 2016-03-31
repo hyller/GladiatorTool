c9f9d1df56eb5cf97b9250d1960e3a5bf38fa6f8
hyller <haile_jiang@163.com>
2016-03-31 15:14:00 +0800

    [Batch] _*_tag.bat

--- Tools/Batch/_isvn_tag.bat
+++ Tools/Batch/_isvn_tag.bat
@@ -1,6 +1,18 @@
 @echo off
 setlocal
 
+set /p input="Did you increase version?    [Y/N]:"
+if not "%input%" == "Y" goto EXIT
+
+set /p input="Did you rebuild project?     [Y/N]:"
+if not "%input%" == "Y" goto EXIT
+
+set /p input="Did you write release notes? [Y/N]:"
+if not "%input%" == "Y" goto EXIT
+
+set /p input="Did you commit all changes?  [Y/N]:"
+if not "%input%" == "Y" goto EXIT
+
 call %GLADIATOR_HOME%\Tools\Batch\_get_version_str.bat %*
 call %GLADIATOR_HOME%\Tools\Batch\_get_cd_name.bat
 
