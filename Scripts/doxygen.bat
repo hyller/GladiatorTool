a0da8e57d74a46d39f4417d8b0775947ba48b59c
hyller <haile.jiang@gmail.com>
2016-06-02 14:41:35 +0800

    [GladiatorFirmware] upgrade doxygen

--- Scripts/doxygen.bat
+++ Scripts/doxygen.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 
-"%GLADIATOR_HOME%\ThirdParty\doxygen-1.8.8.windows.x64.bin\doxygen" %*
+"%GLADIATOR_HOME%\ThirdParty\doxygen-1.8.x.windows.x64.bin\doxygen" %*
 
 endlocal
 @echo on
