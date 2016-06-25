a31bdda0d50570607f10f45dd58fb8757f4c77a9
hyller <haile_jiang@163.com>
2016-06-25 15:53:09 +0800

    [GladiatorFirmware] upgrade semver to 0.11.0

--- Tools/Batch/_iar_arm_build.bat
+++ Tools/Batch/_iar_arm_build.bat
@@ -1,9 +1,9 @@
 REM define ARM build tool path if not defined
 IF NOT DEFINED IARBUILD_EXE_ARM (SET IARBUILD_EXE_ARM="C:\Program Files\IAR Systems\Embedded Workbench ARM 6.30.7\common\bin\IarBuild.exe")
 
-%IARBUILD_EXE_ARM% %1% -clean  Debug
-%IARBUILD_EXE_ARM% %1% -build  Debug
+%IARBUILD_EXE_ARM% %1 -clean  Debug
+%IARBUILD_EXE_ARM% %1 -build  Debug
 
-%IARBUILD_EXE_ARM% %1% -clean  Release
-%IARBUILD_EXE_ARM% %1% -build  Release
+%IARBUILD_EXE_ARM% %1 -clean  Release
+%IARBUILD_EXE_ARM% %1 -build  Release
 
