5d44dcb9ebb0ceb68b168b36ee46055444b7ddc4
hyller <haile_jiang@163.com>
2016-08-04 03:58:00 +0800

    [GladiatorFirmware] trigger ci

--- Tools/Ascii/objs/Test/FormatOutputSpy.d
+++
@@ -1,7 +0,0 @@
-objs/Test//FormatOutputSpy.o: Test//FormatOutputSpy.cpp \
- ../../COTS/CppUTest/2.4/include/CppUTest/MemoryLeakDetectorNewMacros.h \
- ../../COTS/CppUTest/2.4/include/CppUTest/MemoryLeakDetectorMallocMacros.h
-
-../../COTS/CppUTest/2.4/include/CppUTest/MemoryLeakDetectorNewMacros.h:
-
-../../COTS/CppUTest/2.4/include/CppUTest/MemoryLeakDetectorMallocMacros.h:
