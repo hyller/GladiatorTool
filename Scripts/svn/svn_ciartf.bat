dd993b47b164585afdb8b8e7c4327c00b80453d8
haile_jiang@163.com <haile_jiang@163.com>
2017-04-11 16:22:53 +0800

    [GladiatorTool] backup

--- Scripts/svn/svn_ciartf.bat
+++ Scripts/svn/svn_ciartf.bat
@@ -2,6 +2,9 @@
 setlocal
 
 call %GLADIATOR_HOME%\Scripts\svn\_svn_get_artf.bat
+call %GLADIATOR_HOME%\Tools\Batch\_get_cd_name.bat
+
+if "%artf%" == "" set artf=%CURRENT_DIR_NAME%
 
 @echo on
 svn ci -m"[%artf%] %~1"
