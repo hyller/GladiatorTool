a49803d898e601e2c4bb4d915101fd59fe09c94f
haile_jiang@163.com <haile_jiang@163.com>
2017-04-12 10:33:30 +0800

    [GladiatorTool] blabla

--- Scripts/igit.bat
+++ Scripts/igit.bat
@@ -18,7 +18,7 @@ git config --global credential.helper store
 goto END
 
 :CLEAN
-for /f "usebackq tokens=2*" %%i in (`git status -s ^| findstr /r "^\??"`) do rm -rf "%%i"
+for /f "usebackq tokens=2*" %%i in (`git status -s ^| findstr /r "^\??"`) do rm -rf "%%i %%j"
 goto END
 
 :COMMIT
