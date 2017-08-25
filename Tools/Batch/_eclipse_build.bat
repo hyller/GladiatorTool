3c6f6f68cda5d3ec0921ce37f96d054b1e87ed7f
haile_jiang@163.com <haile_jiang@163.com>
2017-08-25 12:11:32 +0800

    backup

--- Tools/Batch/_eclipse_build.bat
+++ Tools/Batch/_eclipse_build.bat
@@ -1,7 +1,11 @@
-@echo off
+REM Build Eclipse Project From Command
+REM Usage: 
+REM Param 1: Project Path
+REM Param 2: Pojectname
+REM Param 3: Config
 
 REM define ARM build tool path if not defined
-IF NOT DEFINED ECLIPSE_EXE (SET ECLIPSE_EXE="C:\Aegis\Core\eclipse.exe")
+IF NOT DEFINED ECLIPSE_EXE (SET ECLIPSE_EXE="C:\Aegis\Core\eclipsec.exe")
 
-%ECLIPSE_EXE% -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild --launcher.suppressErrors -data "tempWorkspace" -import "%1%" -cleanBuild "%1%/Debug"
+%ECLIPSE_EXE% -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild --launcher.suppressErrors -data "tempWorkspace" -import "%1" -cleanbuild "%2/%3"
 rmdir tempWorkspace /s /q
