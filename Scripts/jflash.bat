4f463937d6c081aa62a10e39e21783992a00e6cc
haile_jiang@163.com <haile_jiang@163.com>
2018-06-26 16:40:38 +0800

    [GladiatorTool] backup

--- Scripts/jflash.bat
+++ Scripts/jflash.bat
@@ -1,7 +1,7 @@
 @echo off
 REM jflash project_file download_file
 ECHO Open a project and data file, start auto processing and exit
-"C:\Program Files (x86)\SEGGER\JLink_V610a\JFlash.exe" -openprj"%~1" -open"%~2"  -auto -exit
+"C:\Program Files (x86)\SEGGER\JLink_V632g\JFlash.exe" -openprj"%~1" -open"%~2"  -auto -exit
 IF ERRORLEVEL 1 goto ERROR
 goto END
 :ERROR
---
+++ Scripts/jflash_efr32mg13_d.bat
@@ -0,0 +1,1 @@
+call jflash.bat "%GLADIATOR_HOME%\Scripts\JFlashPro\EFR32MG13_D.jflash" "%~1"
