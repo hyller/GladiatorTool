febe46d1842ccbe3c2cf33d7fd586a07b73d4fea
haile_jiang@163.com <haile_jiang@163.com>
2019-02-19 13:45:05 +0800

    [GladiatorTool] backup

---
+++ Scripts/makeiararm7804.bat
@@ -0,0 +1,11 @@
+REM Build EWARM Project From Command
+REM Usage: 
+REM Param 1: .ewp ProjectPath
+REM Param 2: Config
+
+REM define ARM build tool path if not defined
+IF NOT DEFINED IARBUILD_EXE_ARM (SET IARBUILD_EXE_ARM="C:\Program Files\IAR Systems\Embedded Workbench ARM 7.80.4\common\bin\IarBuild.exe")
+
+%IARBUILD_EXE_ARM% %1 -make %2
+
+
