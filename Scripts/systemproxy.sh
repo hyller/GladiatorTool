b97f3d425a22391c09041dbfe7a653348523b678
sesa280753 <haile-hyller.jiang@se.com>
2020-07-23 13:17:42 +0800

    [Scripts] backup

---
+++ Scripts/systemproxy.sh
@@ -0,0 +1,5 @@
+# export http_proxy=username:password@proxy-server-ip:8080
+# export https_proxy=username:password@proxy-server-ip:8082
+# export ftp_proxy=username:password@proxy-server-ip:8080
+export http_proxy=http://58.220.95.9:80/
+export https_proxy=http://58.220.95.9:80/
