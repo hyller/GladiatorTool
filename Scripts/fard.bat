5b45a10f4588951fbbfb61e6662724730b694a31
sesa280753 <haile-hyller.jiang@se.com>
2020-08-19 09:02:11 +0800

    backup

--- Scripts/fard.bat
+++ Scripts/fard.bat
@@ -5,8 +5,10 @@ rem brief: Replace string in all files under current directly
 rem usage: fard str_old str_new
 rem ex   : fard gaia gesp
 rem        replace gaia by gesp
+rem to be safe, only do find in this script, if you want to find and replace, please use `fart -r --svn . old new`
+rem 
 
-fart -r --svn . %1 %2
+fart -r --svn . %1
 
 endlocal
 @echo on
