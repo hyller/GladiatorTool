f797414172abe47d7ec939774b1c073f6e96f9c2
hyller <haile_jiang@163.com>
2023-06-15 11:28:56 +0800

    Update systemproxy.sh

--- Scripts/systemproxy.sh
+++ Scripts/systemproxy.sh
@@ -1,5 +1,1 @@
-# export http_proxy=username:password@proxy-server-ip:8080
-# export https_proxy=username:password@proxy-server-ip:8082
-# export ftp_proxy=username:password@proxy-server-ip:8080
-export http_proxy=http://10.198.157.116:9400/
-export https_proxy=http://10.198.157.116:9400/
+
